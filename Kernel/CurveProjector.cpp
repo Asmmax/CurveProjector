48e0fc368f60c02a2ad3b674ec718786741eda08
Nikita Morunov <niknixad@gmail.com>
2021-10-20 18:08:19 +0400

    Project files are added

---
+++ Kernel/CurveProjector.cpp
@@ -0,0 +1,12 @@
+﻿// CurveProjector.cpp: определяет точку входа для приложения.
+//
+
+#include "CurveProjector.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
