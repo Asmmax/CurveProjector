17d08b18127e518b40f0374d281b56b9e2951f1f
Nikita Morunov <niknixad@gmail.com>
2021-10-21 11:34:06 +0400

    Test for point-to-polyline project with many point is added

--- KernelTests/PolylineTests.cpp
+++ KernelTests/PolylineTests.cpp
@@ -24,4 +24,27 @@ TEST_CASE("Point to polyline projection", "[Polyline]") {
 	Point requirePoint{ 2.0 , 1.0, 2.0 };
 	REQUIRE(projection.point.epsilon() < 1e-10);
 	REQUIRE(projection.point == requirePoint);
+
+	SECTION("Case of many point projections")
+	{
+		std::vector<Point> points;
+		points.emplace_back(2.0, 0.0, 0.0);
+		points.emplace_back(2.0, 2.0, 0.0);
+		points.emplace_back(0.0, 2.0, 0.0);
+
+		Polyline somePolyline{ points };
+		Point somePoint{ 1.0, 1.0, 0.0 };
+
+		std::vector<Polyline::Projection> projections = somePolyline.project(somePoint);
+		REQUIRE(projections.size() == 2);
+
+		Point requirePoint1{ 2.0 , 1.0, 0.0 };
+		REQUIRE(projections[0].point.epsilon() < 1e-10);
+		REQUIRE(projections[0].point == requirePoint1);
+
+		Point requirePoint2{ 1.0 , 2.0, 0.0 };
+		REQUIRE(projections[1].point.epsilon() < 1e-10);
+		REQUIRE(projections[1].point == requirePoint2);
+
+	}
 }
