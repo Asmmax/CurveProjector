617c58f7e91e8914fde84e231a5ff05038634916
Nikita Morunov <niknixad@gmail.com>
2021-10-22 14:44:12 +0400

    ConsoleApp refactoring - Context removed

--- ConsoleApp/src/main.cpp
+++ ConsoleApp/src/main.cpp
@@ -1,33 +1,9 @@
-#include "Polyline.hpp"
-#include "Point.hpp"
-#include "Context.hpp"
-#include "PolylineGateway.hpp"
-#include "Parser.hpp"
-#include <string>
-#include <iostream>
+#include "ConsoleApp.hpp"
+#include "PolylineFile.hpp"
 
 int main(int argc, char* argv[])
 {
-	if (argc < 5) {
-		std::cout << "Error input!" << std::endl;
-		std::cout << "You need to input 4 arguments" << std::endl;
-		std::cout << "[file] [x] [y] [z]" << std::endl;
-		std::cout << "file - text file with polyline points" << std::endl;
-		std::cout << "x,y,z - values of three dimensional point" << std::endl;
-		std::cout << "Example: \"file.txt\" 0 1 2.5" << std::endl;
-		return 1;
-	}
-
-	Context context;
-	auto* polylineFab = context.polylineGateway();
-
-	auto polyline = polylineFab->getPolyline(argv[1]);
-	auto point = Parser::toPoint(argv[2], argv[3], argv[4]);
-
-	auto projections = polyline->project(point);
-
-	std::cout << "Projection has: " << projections.size() << " points" << std::endl;
-	std::cout << Parser::toString(projections);
-
-	return 0;
+	PolylineFile polylineFile;
+	ConsoleApp app(polylineFile);
+	return app.run(argc, argv);
 }
